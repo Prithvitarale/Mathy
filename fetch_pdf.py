114710e67f3f41ab23e74e471f94638b0f40ad38
Prithvi <pritht@uw.edu>
2021-04-16 16:07:22 -0700

    processed mathy

--- fetch_pdf.py
+++ fetch_pdf.py
@@ -2,6 +2,22 @@
 usage: python fetch_pdf.py --url_file [filename] --out_dir [papers_raw/mathy]
 assign ids
 """
+from urllib.request import urlretrieve
+import sys
+import os
+def load_data(url_file, out_dir):
+    filename = url_file
+    f = open(filename, encoding="utf8")
+    links = f.readlines()
+    os.mkdir(out_dir)
+    for i, link in enumerate(links):
+        print(i)
+        path = os.path.join(out_dir, f"{i}.pdf")
+        urlretrieve(link.strip(), path)
 
-
-
+print(sys.argv)
+os.mkdir(".\\papers_raw")
+load_data(sys.argv[2], sys.argv[4])
+# conda env create -f environment.yml
+# conda create -n control
+# conda activate control
--- process_pdf.py
+++ process_pdf.py
@@ -2,3 +2,22 @@
 usage: python process_pdf.py --input_dir [papers_raw/mathy] --output_dir [papers_processed/mathy]
 
 """
+import pdftotext
+import glob
+import os
+import sys
+def process_pdf(input_dir, output_dir):
+    for i, pdf_file in enumerate(glob.glob(input_dir)):
+        pdf = open(pdf_file, "rb")
+        # print(i)
+        text = pdftotext.PDF(pdf)
+        pdfintext = ""
+        for page in text:
+            pdfintext += page + "\n"
+        output_filename = os.path.join(output_dir, f"{i}.txt")
+        f = open(output_filename, "w+", encoding="utf8")
+        f.write(pdfintext)
+        f.close()
+os.mkdir(".\\papers_processed")
+os.mkdir(".\\papers_processed\\mathy")
+process_pdf(sys.argv[2], sys.argv[4])
